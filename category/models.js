f6afb1b71a12f5ebdf14a4226003d4a24c9e7402
Russ yev <ruslanyev@gmail.com>
2019-01-29 15:17:18 -0800

    updated schema to include array for business ID's

--- category/models.js
+++ category/models.js
@@ -4,12 +4,14 @@ const mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 
 const CategorySchema = mongoose.Schema({
-  name: {type: String, required: true}
+  name: {type: String, required: true},
+  business: [{type: mongoose.Schema.Types.ObjectId, ref: 'Business'}]
 });
 
 CategorySchema.methods.serialize = function () {
   return {
-    name: this.name
+    name: this.name,
+    business: this.business
   };
 };
 
