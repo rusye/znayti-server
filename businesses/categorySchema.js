ee5fd7dfe4bd1bc36cda1d57f2c79f7c47f2e516
Russ yev <ruslanyev@gmail.com>
2019-01-28 20:09:41 -0800

    seperated category schema

---
+++ businesses/categorySchema.js
@@ -0,0 +1,18 @@
+'use strict';
+const mongoose = require('mongoose');
+
+mongoose.Promise = global.Promise;
+
+const CategorySchema = mongoose.Schema({
+  name: {type: String, required: true}
+});
+
+CategorySchema.methods.serialize = function () {
+  return {
+    name: this.name || ''
+  };
+};
+
+const Category = mongoose.model('Category', CategorySchema);
+
+module.exports = {Category};
