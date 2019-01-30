d7c0c883bfcb930f09584617c8cb6b3adf48c9b9
Russ yev <ruslanyev@gmail.com>
2019-01-29 18:31:32 -0800

    add all CRUD endpoints

--- category/router.js
+++ category/router.js
@@ -12,71 +12,44 @@ const router = express.Router();
 // passport.use(jwtStrategy);
 // const jwtAuth = passport.authenticate('jwt', {session: false});
 
-// Do I need to import all of them?
-const {Business} = require('./models');
-const {Category} = require('./categorySchema');
+const {Category} = require('./models');
 const {User} = require('../users/models');
 
 
 // GET request for all categories
 router.get('/', (req, res) => {
   Category.find()
+    .populate('business')
     .then(categories => res.json(categories.map(category => category.serialize())))
-    .catch(err => res.status(500).json({message: 'Internal server error'}));
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({message: 'Internal server error'})
+    });
 });
 
 
 // ---- Require jwtAuth later ----
 // POST request to create a new business
 router.post('/', (req, res) => {
-  const requiredFields = ['name', 'categoryName', 'address', 'hours', 'tel'];
-  requiredFields.forEach(field => {
-    if (!(field in req.body)) {
-      const message = `Missing \`${field}\` in request body`;
-      console.error(message);
-      return res.status(400).send(message);
-    }
-  });
+  if (!('name' in req.body)) {
+    const message = `Missing name in request body`;
+    console.error(message);
+    return res.status(400).send(message);
+  };
 
-  // I forgot but where will it compare the req to the schema?
   User.findById(req.body.user_id)
     .then(user => {
       if (user) {
-        Business
+        Category
           .create({
             user: req.body.user_id,
-            name: req.body.name,
-            categoryName: req.body.categoryName,
-            address: req.body.address,
-            hours: req.body.hours,
-            tel: req.body.tel
+            name: req.body.name
           })
-          .then(business => {
-            // Category.update(
-            //   { $push: { category: business.category } }
-            // );
-            // I'm not sure if the above works correctly
-            // Basically I need to check if that "Category first exists"
-            // If it doesn't, it will create a new document
-            // Then it will push the business.id to the new document
-            // Would it be something like
-            // Category .find(business.category).then(category => {
-            //   if(category) {
-            //     category.category.push(business._id)
-            //   } else {
-            //     Category.create({category: business.category}).then(category => {
-            //       category.category.push(business._id)
-            //     })
-            //   }
-            // })
-            business.save();
+          .then(category => {
+            category.save();
             res.status(201).json({
-              id: business.id,
-              name: business.name,
-              category: business.category,
-              address: business.address,
-              hours: business.hours,
-              tel: business.tel
+              id: category.id,
+              name: category.name
             })
           })
           .catch(err => {
@@ -90,13 +63,42 @@ router.post('/', (req, res) => {
         return res.status(400).send(message);
       }
     })
-    
     .catch(err => {
       console.error(err);
       res.status(500).json({message: 'Something went wrong'});
     });
 });
 
-// Make a Business GET request above the POST
+
+// ---- Require jwtAuth later ----
+// PUT request to update a business
+router.put('/:id', (req, res) => {
+  if (!(req.params.id && req.body.id && req.params.id === req.body.id)) {
+    res.status(400).json({message: `ID's do not match`});
+  }
+
+  const toUpdate = {};
+  const updateableFields = ['name'];
+  updateableFields.forEach(field => {
+    if (field in req.body) {
+      toUpdate[field] = req.body[field];
+    }
+  });
+
+  Category
+    .findByIdAndUpdate(req.params.id, {$set: toUpdate}, {new: true})
+    .then(updatedPost => res.status(204).end())
+    .catch(err => res.status(500).json({message: 'Something went wrong'}));
+});
+
+
+// ---- Require jwtAuth later ----
+// DELETE request to delete a category
+router.delete('/:id', (req, res) => {
+  Category
+    .findByIdAndRemove(req.params.id)
+    .then(post => res.status(204).end())
+    .catch(err => res.status(500).json({message: "Internal server error"}));
+});
 
 module.exports = {router};
