93d8f7157ddcee36338fc2942b2cafc2725e06ad
Russ yev <ruslanyev@gmail.com>
2019-02-03 16:06:40 -0800

    added admin role as a private claim

--- auth/router.js
+++ auth/router.js
@@ -8,7 +8,7 @@ const config = require('../config');
 const router = express.Router();
 
 const createAuthToken = function(user) {
-  return jwt.sign({user}, config.JWT_SECRET, {
+  return jwt.sign({user, "admin": user.admin}, config.JWT_SECRET, {
     subject: user.username,
     expiresIn: config.JWT_EXPIRY,
     algorithm: 'HS256'
@@ -19,8 +19,9 @@ const localAuth = passport.authenticate('local', {session: false});
 router.use(bodyParser.json());
 // The user provides a username and password to login
 router.post('/login', localAuth, (req, res) => {
-  const authToken = createAuthToken(req.user.serialize());
-  res.json({authToken});
+  const user = req.user.serialize();
+  const authToken = createAuthToken(user);
+  res.json({authToken, user});
 });
 
 const jwtAuth = passport.authenticate('jwt', {session: false});
