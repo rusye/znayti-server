26a66713cdb9656d7aac6bc5ebf82da24277f7a1
Russ yev <ruslanyev@gmail.com>
2019-02-07 14:25:14 -0800

    removed businesses array

--- category/models.js
+++ category/models.js
@@ -4,14 +4,12 @@ const mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 
 const CategorySchema = mongoose.Schema({
-  name: {type: String, required: true},
-  business: [{type: mongoose.Schema.Types.ObjectId, ref: 'Business'}]
+  name: {type: String, required: true}
 });
 
 CategorySchema.methods.serialize = function () {
   return {
-    name: this.name,
-    business: this.business
+    name: this.name
   };
 };
 
