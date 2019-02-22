b51310661f7d34a18f363a782362b664d770a959
Russ yev <ruslanyev@gmail.com>
2019-02-22 10:18:41 -0800

    cleaned up code, revised validation response from ###'s to digits

--- businesses/models.js
+++ businesses/models.js
@@ -3,12 +3,12 @@ const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
 
-const statesArray = ["AL", "AK", "AZ", "AR", "CA", "CO", "CT", "DC", "DE", "FL", "GA", "HI", "ID", "IL", "IN", "IA", "KS", "KY", "LA", "ME", "MD", "MA", "MI", "MN", "MS", "MO", "MT", "NE", "NV", "NH", "NJ", "NM", "NY", "NC", "ND", "OH", "OK", "OR", "PA", "RI", "SC", "SD", "TN", "TX", "UT", "VT", "VA", "WA", "WV", "WI", "WY"];
+const statesArray = ['AL', 'AK', 'AZ', 'AR', 'CA', 'CO', 'CT', 'DC', 'DE', 'FL', 'GA', 'HI', 'ID', 'IL', 'IN', 'IA', 'KS', 'KY', 'LA', 'ME', 'MD', 'MA', 'MI', 'MN', 'MS', 'MO', 'MT', 'NE', 'NV', 'NH', 'NJ', 'NM', 'NY', 'NC', 'ND', 'OH', 'OK', 'OR', 'PA', 'RI', 'SC', 'SD', 'TN', 'TX', 'UT', 'VT', 'VA', 'WA', 'WV', 'WI', 'WY'];
 
 let telephoneValidate = function(v, cb) {
   setTimeout(function() {
     let phoneRegex = /\d{3}-\d{3}-\d{4}/;
-    let msg = v + ' is not a valid phone number! Must be ###-###-####';
+    let msg = v + ' is not a valid phone number! Must be 123-123-1234';
     cb(phoneRegex.test(v), msg);
   }, 5);
 }
@@ -16,7 +16,7 @@ let telephoneValidate = function(v, cb) {
 const hoursValidation = function(v, cb) {
   setTimeout(function() {
     let hoursRegex = /^(open)\s([01]?[0-9]|2[0-3]):([0-5][0-9])\s(close)\s([01]?[0-9]|2[0-3]):([0-5][0-9])|(closed)$/;
-    let msg = v + ' is not a valid hours format! Must be "open ##:## close ##:##" or "closed"';
+    let msg = v + ' is not a valid hours format! Must be "open 01:12 close 12:12" or "closed"';
     cb(hoursRegex.test(v), msg);
   }, 5)
 }
