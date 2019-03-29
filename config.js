c3313d0113cb4015f0b0fe3887e42a9d47ecef84
Russ yev <ruslanyev@gmail.com>
2019-03-28 18:24:25 -0700

    switched to google api from mapquest

--- config.js
+++ config.js
@@ -1,7 +1,8 @@
-'use strict';
-exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://localhost/znayti';
-exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL || 'mongodb://localhost/test-znayti';
+"use strict";
+exports.DATABASE_URL = process.env.DATABASE_URL || "mongodb://localhost/znayti";
+exports.TEST_DATABASE_URL =
+  process.env.TEST_DATABASE_URL || "mongodb://localhost/test-znayti";
 exports.PORT = process.env.PORT || 8080;
 exports.JWT_SECRET = process.env.JWT_SECRET;
-exports.JWT_EXPIRY = process.env.JWT_EXPIRY || '7d';
-exports.MAPQUEST_API = process.env.MAPQUEST_API;
+exports.JWT_EXPIRY = process.env.JWT_EXPIRY || "7d";
+exports.GOOGLEMAPS_API = process.env.GOOGLEMAPS_API;
