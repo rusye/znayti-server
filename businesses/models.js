c952aa580e0a715f666e8c670579ce3274a873c8
Russ yev <ruslanyev@gmail.com>
2019-03-28 18:21:37 -0700

    added googlePlace and contactName to schema

--- businesses/models.js
+++ businesses/models.js
@@ -1,105 +1,164 @@
-'use strict';
-const mongoose = require('mongoose');
+"use strict";
+const mongoose = require("mongoose");
 
 mongoose.Promise = global.Promise;
 
-const statesArray = ['AL', 'AK', 'AZ', 'AR', 'CA', 'CO', 'CT', 'DC', 'DE', 'FL', 'GA', 'HI', 'ID', 'IL', 'IN', 'IA', 'KS', 'KY', 'LA', 'ME', 'MD', 'MA', 'MI', 'MN', 'MS', 'MO', 'MT', 'NE', 'NV', 'NH', 'NJ', 'NM', 'NY', 'NC', 'ND', 'OH', 'OK', 'OR', 'PA', 'RI', 'SC', 'SD', 'TN', 'TX', 'UT', 'VT', 'VA', 'WA', 'WV', 'WI', 'WY'];
+const statesArray = [
+  "AL",
+  "AK",
+  "AZ",
+  "AR",
+  "CA",
+  "CO",
+  "CT",
+  "DC",
+  "DE",
+  "FL",
+  "GA",
+  "HI",
+  "ID",
+  "IL",
+  "IN",
+  "IA",
+  "KS",
+  "KY",
+  "LA",
+  "ME",
+  "MD",
+  "MA",
+  "MI",
+  "MN",
+  "MS",
+  "MO",
+  "MT",
+  "NE",
+  "NV",
+  "NH",
+  "NJ",
+  "NM",
+  "NY",
+  "NC",
+  "ND",
+  "OH",
+  "OK",
+  "OR",
+  "PA",
+  "RI",
+  "SC",
+  "SD",
+  "TN",
+  "TX",
+  "UT",
+  "VT",
+  "VA",
+  "WA",
+  "WV",
+  "WI",
+  "WY"
+];
 
 let telephoneValidate = function(v, cb) {
   setTimeout(function() {
     let phoneRegex = /^[0-9]{10,10}$/;
-    let msg = v + ' is not a valid phone number! Must be 1234567890';
+    let msg = v + " is not a valid phone number! Must be 1234567890";
     cb(phoneRegex.test(v), msg);
   }, 5);
-}
+};
 
 const hoursValidation = function(v, cb) {
-  if(v.toString()=="") {
-    return true
+  if (v.toString() == "") {
+    return true;
   } else {
     let hoursRegex = /^([0-1][0-9]|2[0-3]):([0-5][0-9])$/;
-    let msg = v + ' is not a valid hours format! Must be between 00:00 and 23:59 or ""';
-    cb(hoursRegex.test(v), msg)
-  };
-}
+    let msg =
+      v + ' is not a valid hours format! Must be between 00:00 and 23:59 or ""';
+    cb(hoursRegex.test(v), msg);
+  }
+};
 
 const hoursLogic = {
   type: String,
   validate: {
     isAsync: true,
     validator: hoursValidation,
-    message: 'Default error message'
+    message: "Default error message"
   },
-  default: ''
+  default: ""
 };
 
 const longitude = {
   type: Number,
   required: true
-}
+};
 const latitude = {
   type: Number,
   required: true
-}
+};
 
 let zipValidation = function(v, cb) {
   setTimeout(function() {
     let zipRegex = /^\d{5}(?:[-]\d{4})?$/;
-    let msg = v + ' is not a valid zip code! Must be in this format 12345 or 12345-1234';
+    let msg =
+      v +
+      " is not a valid zip code! Must be in this format 12345 or 12345-1234";
     cb(zipRegex.test(v), msg);
   }, 5);
-}
+};
 
 const zipCode = {
   type: String,
   validate: {
     isAsync: true,
     validator: zipValidation,
-    message: 'Default error message'
+    message: "Default error message"
   }
-}
+};
 
 const BusinessSchema = mongoose.Schema({
-  user: {type: mongoose.Schema.Types.ObjectId, ref: 'User'},
-  name: {type: String, required: true},
-  category: {type: mongoose.Schema.Types.ObjectId, ref: 'Category'},
+  user: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+  name: { type: String, required: true },
+  contactName: String,
+  category: { type: mongoose.Schema.Types.ObjectId, ref: "Category" },
+  googlePlace: { type: String, required: true },
   address: {
-    street: {type: String, required: true},
-    city: {type: String, required: true},
+    street: { type: String, required: true },
+    city: { type: String, required: true },
     state: {
       type: String,
       uppercase: true,
       enum: statesArray,
-      required: true},
+      required: true
+    },
     zip: zipCode,
     coordinates: [longitude, latitude]
   },
   hours: {
-    Monday: {open: hoursLogic, close: hoursLogic},
-    Tuesday: {open: hoursLogic, close: hoursLogic},
-    Wednesday: {open: hoursLogic, close: hoursLogic},
-    Thursday: {open: hoursLogic, close: hoursLogic},
-    Friday: {open: hoursLogic, close: hoursLogic},
-    Saturday: {open: hoursLogic, close: hoursLogic},
-    Sunday: {open: hoursLogic, close: hoursLogic}
+    Monday: { open: hoursLogic, close: hoursLogic },
+    Tuesday: { open: hoursLogic, close: hoursLogic },
+    Wednesday: { open: hoursLogic, close: hoursLogic },
+    Thursday: { open: hoursLogic, close: hoursLogic },
+    Friday: { open: hoursLogic, close: hoursLogic },
+    Saturday: { open: hoursLogic, close: hoursLogic },
+    Sunday: { open: hoursLogic, close: hoursLogic }
   },
   telephone: {
     type: String,
     validate: {
       isAsync: true,
       validator: telephoneValidate,
-      message: 'Default error message'
+      message: "Default error message"
     },
     required: true
   }
 });
 
-
-BusinessSchema.methods.serialize = function () {
+BusinessSchema.methods.serialize = function() {
   return {
     id: this._id,
     user: this.user,
     name: this.name,
+    contactName: this.contactName,
+    googlePlace: this.googlePlace,
     category: this.category,
     address: this.address,
     hours: this.hours,
@@ -107,14 +166,14 @@ BusinessSchema.methods.serialize = function () {
   };
 };
 
-BusinessSchema.pre('find', function() {
-  this.populate('category')
-})
+BusinessSchema.pre("find", function() {
+  this.populate("category");
+});
 
-BusinessSchema.pre('findOne', function() {
-  this.populate('category')
-})
+BusinessSchema.pre("findOne", function() {
+  this.populate("category");
+});
 
-const Business = mongoose.model('Business', BusinessSchema);
+const Business = mongoose.model("Business", BusinessSchema);
 
-module.exports = {Business};
+module.exports = { Business };
--- businesses/router.js
+++ businesses/router.js
@@ -1,327 +1,373 @@
-'use strict';
+"use strict";
 
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
 // ---- activate passport later ----
 // const passport = require('passport');
 
-// ---- require passport/authentication later ---- 
+// ---- require passport/authentication later ----
 // const { router: localStrategy, jwtStrategy, isAdmin } = require('./auth');
 // passport.use(localStrategy);
 // passport.use(jwtStrategy);
 // const jwtAuth = passport.authenticate('jwt', {session: false});
 
-const {Business} = require('./models');
-const {Category} = require('../category/models');
-const {User} = require('../users/models');
+const { Business } = require("./models");
+const { Category } = require("../category/models");
+const { User } = require("../users/models");
 
 // GET request for categories in an area
-router.get('/search', (req, res) => {
-  Business
-    .find({'address.coordinates': {$geoWithin: { $centerSphere: [[req.query.long, req.query.lat ], req.query.rad/3963.2]}}})
+router.get("/search", (req, res) => {
+  Business.find({
+    "address.coordinates": {
+      $geoWithin: {
+        $centerSphere: [[req.query.long, req.query.lat], req.query.rad / 3963.2]
+      }
+    }
+  })
     .then(categories => {
-      let catNames = []
+      let catNames = [];
       for (let i = 0; i < categories.length; i++) {
         if (catNames.indexOf(categories[i].category.name) === -1) {
-          catNames.push(categories[i].category.name)
+          catNames.push(categories[i].category.name);
         }
       }
-      res.json(catNames.sort())
+      res.json(catNames.sort());
     })
     .catch(err => {
       console.log(err);
-      res.status(500).json({message: 'Internal server error'})
+      res.status(500).json({ message: "Internal server error" });
     });
 });
 
 // GET request for businesses within a category
-router.get('/:category/search', (req, res) => {
-  Category
-    .find({name: req.params.category})
+router.get("/:category/search", (req, res) => {
+  Category.find({ name: req.params.category })
     .then(cat => {
-      if(cat.length === 0) {
-        const message = '404 Category not found';
+      if (cat.length === 0) {
+        const message = "404 Category not found";
         console.error(message);
-        return res.status(404).json({code: 404, message});
+        return res.status(404).json({ code: 404, message });
       } else {
-        Business
-          .find({'address.coordinates': {$geoWithin: { $centerSphere: [[req.query.long, req.query.lat ], req.query.rad/3963.2]}}})
-          .sort('name')
+        Business.find({
+          "address.coordinates": {
+            $geoWithin: {
+              $centerSphere: [
+                [req.query.long, req.query.lat],
+                req.query.rad / 3963.2
+              ]
+            }
+          }
+        })
+          .sort("name")
           .then(businesses => {
-            let items = businesses.filter(business => business.category.name === req.params.category)
-            res.json(items.map(item => {
-              return {
-                id: item._id,
-                name: item.name,
-                city: item.address.city,
-                state: item.address.state
-              }
-            }))
+            let items = businesses.filter(
+              business => business.category.name === req.params.category
+            );
+            res.json(
+              items.map(item => {
+                return {
+                  id: item._id,
+                  name: item.name,
+                  contactName: item.contactName,
+                  city: item.address.city,
+                  state: item.address.state
+                };
+              })
+            );
           })
           .catch(err => {
             console.log(err);
-            res.status(500).json({message: 'Internal server error'})
+            res.status(500).json({ message: "Internal server error" });
           });
       }
     })
     .catch(err => {
       console.log(err);
-      res.status(500).json({message: 'Internal server error'})
+      res.status(500).json({ message: "Internal server error" });
     });
 });
 
 // GET request for a business
-router.get('/:id', (req, res) => {
+router.get("/:id", (req, res) => {
   Business.findById(req.params.id)
     .then(count => {
       if (count === null) {
         return Promise.reject({
           code: 404,
-          reason: 'ValidationError',
-          message: '404 Business not found',
-          location: 'name'
+          reason: "ValidationError",
+          message: "404 Business not found",
+          location: "name"
         });
       }
-      return count
+      return count;
     })
     .then(business => res.json(business.serialize()))
     .catch(err => {
       console.log(err);
-      if (err.reason === 'ValidationError') {
+      if (err.reason === "ValidationError") {
         return res.status(err.code).json(err);
       }
-      res.status(500).json({message: 'Internal server error'})
+      res.status(500).json({ message: "Internal server error" });
     });
 });
-
 
 // FUTURE UPDATE: Figure out how to check for business name also
 // ---- Require jwtAuth later ----
 // POST request to create a new business
-router.post('/', (req, res) => {
-  let requiredFields = ['name', 'category', 'address', 'hours', 'telephone'];
+router.post("/", (req, res) => {
+  let requiredFields = [
+    "name",
+    "category",
+    "address",
+    "hours",
+    "telephone",
+    "googlePlace"
+  ];
   requiredFields.forEach(field => {
     if (!(field in req.body)) {
       const message = `Missing \`${field}\` in request body`;
       console.error(message);
-      return res.status(400).json({code: 400, message});
+      return res.status(400).json({ code: 400, message });
     }
   });
 
   function isLatitude(lat) {
-    if (isNaN(lat)) return true
-    if (lat >= -0 && lat <= 90) return true
+    if (isNaN(lat)) return true;
+    if (lat >= -0 && lat <= 90) return true;
   }
 
-  if(!isLatitude(req.body.address.coordinates[1])) {
+  if (!isLatitude(req.body.address.coordinates[1])) {
     return res.status(422).json({
       code: 422,
-      reason: 'ValidationError',
-      message: 'Latitude must be between 0 and 90',
-      location: 'Coordinates'
+      reason: "ValidationError",
+      message: "Latitude must be between 0 and 90",
+      location: "Coordinates"
     });
   }
-  
+
   function isLongitude(long) {
-    if (isNaN(long)) return true
-    if(long >= -180 && long <= 0) return true
+    if (isNaN(long)) return true;
+    if (long >= -180 && long <= 0) return true;
   }
 
-  if(!isLongitude(req.body.address.coordinates[0])) {
+  if (!isLongitude(req.body.address.coordinates[0])) {
     return res.status(422).json({
       code: 422,
-      reason: 'ValidationError',
-      message: 'Longitude must be between -180 and 0',
-      location: 'Coordinates'
+      reason: "ValidationError",
+      message: "Longitude must be between -180 and 0",
+      location: "Coordinates"
     });
   }
 
   User.findById(req.body.user_id)
     .then(user => {
       if (user) {
-        Business.find({'name': req.body.name, 'address.street': req.body.address.street})
-        .count()
-        .then(count => {
-          if (count > 0) {
-            return Promise.reject({
-              code: 422,
-              reason: 'ValidationError',
-              message: 'This business already exists at this address',
-              location: 'name and address'
-            });
-          }
-          return count
+        Business.find({
+          name: req.body.name,
+          "address.street": req.body.address.street,
+          telephone: req.body.telephone
         })
-        .then(category => {
-          Category
-            .findById(req.body.category)
-            .count()
-            .then(count => {
-              if (count === 0) {
-                return Promise.reject({
-                  code: 400,
-                  reason: 'ValidationError',
-                  message: 'Category doesn\'t exist',
-                  location: 'category name'
-                });
-              }
-              return count
-            })
-            .then(business => {
-              return Business.create({
-                user: req.body.user_id,
-                name: req.body.name,
-                category: req.body.category,
-                address: req.body.address,
-                hours: req.body.hours,
-                telephone: req.body.telephone
-              })
-            })
-            .then(business => {
-              res.status(201).json({
-                id: business.id,
-                name: business.name,
-                category: business.category,
-                address: business.address,
-                hours: business.hours,
-                telephone: business.telephone
+          .count()
+          .then(count => {
+            if (count > 0) {
+              return Promise.reject({
+                code: 422,
+                reason: "ValidationError",
+                message: "This business already exists at this address",
+                location: "name and address"
               });
-            })
-            .catch(err => {
-              console.error(err);
-              if (err.reason === 'ValidationError') {
-                return res.status(err.code).json(err);
-              }
-              res.status(500).json({code: 500, message: 'Internal server error'});
-            });
-        })
-        .catch(err => {
-          console.error(err);
-          if (err.reason === 'ValidationError') {
-            return res.status(err.code).json(err);
-          }
-          res.status(500).json({code: 500, message: 'Internal server error'});
-        });
-      }
-      else {
-        const message = 'User not found';
+            }
+            return count;
+          })
+          .then(category => {
+            Category.findById(req.body.category)
+              .count()
+              .then(count => {
+                if (count === 0) {
+                  return Promise.reject({
+                    code: 400,
+                    reason: "ValidationError",
+                    message: "Category doesn't exist",
+                    location: "category name"
+                  });
+                }
+                return count;
+              })
+              .then(business => {
+                return Business.create({
+                  user: req.body.user_id,
+                  name: req.body.name,
+                  contactName: req.body.contactName,
+                  googlePlace: req.body.googlePlace,
+                  category: req.body.category,
+                  address: req.body.address,
+                  hours: req.body.hours,
+                  telephone: req.body.telephone
+                });
+              })
+              .then(business => {
+                res.status(201).json({
+                  id: business.id,
+                  name: business.name,
+                  contactName: business.contactName,
+                  googlePlace: business.googlePlace,
+                  category: business.category,
+                  address: business.address,
+                  hours: business.hours,
+                  telephone: business.telephone
+                });
+              })
+              .catch(err => {
+                console.error(err);
+                if (err.reason === "ValidationError") {
+                  return res.status(err.code).json(err);
+                }
+                res
+                  .status(500)
+                  .json({ code: 500, message: "Internal server error" });
+              });
+          })
+          .catch(err => {
+            console.error(err);
+            if (err.reason === "ValidationError") {
+              return res.status(err.code).json(err);
+            }
+            res
+              .status(500)
+              .json({ code: 500, message: "Internal server error" });
+          });
+      } else {
+        const message = "User not found";
         console.error(message);
-        return res.status(400).json({code: 400, message});
+        return res.status(400).json({ code: 400, message });
       }
     })
     .catch(err => {
       console.error(err);
-      res.status(500).json({message: 'Something went wrong'});
+      res.status(500).json({ message: "Something went wrong" });
     });
 });
 
-
 // ---- Require jwtAuth later ----
 // PUT request to update a business
-router.put('/:id', (req, res) => {
+router.put("/:id", (req, res) => {
   if (!(req.params.id && req.body.id && req.params.id === req.body.id)) {
-    res.status(400).json({message: `ID's do not match`});
+    res.status(400).json({ message: `ID's do not match` });
   }
 
-  let requiredFields = ['name','address', 'user'];
+  let requiredFields = ["name", "address", "user"];
   requiredFields.forEach(field => {
     if (!(field in req.body)) {
       const message = `Missing \`${field}\` in request body`;
       console.error(message);
-      return res.status(400).json({code: 400, message});
+      return res.status(400).json({ code: 400, message });
     }
   });
 
   const toUpdate = {};
-  const updateableFields = ['user', 'name', 'category', 'address', 'hours', 'telephone'] 
+  const updateableFields = [
+    "user",
+    "name",
+    "category",
+    "address",
+    "hours",
+    "telephone",
+    "contactName",
+    "googlePlace"
+  ];
   updateableFields.forEach(field => {
     if (field in req.body) {
       toUpdate[field] = req.body[field];
     }
   });
 
-  function dotNotate(obj,target,prefix) {
-    target = target || {},
-    prefix = prefix || '';
-  
+  function dotNotate(obj, target, prefix) {
+    (target = target || {}), (prefix = prefix || "");
+
     Object.keys(obj).forEach(function(key) {
-      if ( typeof(obj[key]) === 'object' ) {
-        dotNotate(obj[key],target,prefix + key + '.');
+      if (typeof obj[key] === "object") {
+        dotNotate(obj[key], target, prefix + key + ".");
       } else {
-        return target[prefix + key] = obj[key];
+        return (target[prefix + key] = obj[key]);
       }
     });
-  
+
     return target;
   }
 
-  const theUpdate = dotNotate(toUpdate)
+  const theUpdate = dotNotate(toUpdate);
 
-  Business
-    .find({'name': req.body.name, 'address.street': req.body.address.street})
+  Business.find({
+    name: req.body.name,
+    "address.street": req.body.address.street,
+    telephone: req.body.telephone
+  })
     .then(res => {
-      if(res.length === 0) {
-        return res
+      if (res.length === 0) {
+        return res;
       }
 
       if (!(`${res[0]._id}` === req.body.id)) {
         return Promise.reject({
           code: 422,
-          reason: 'ValidationError',
-          message: 'This business already exists at this address',
-          location: 'name and address'
+          reason: "ValidationError",
+          message: "This business already exists at this address",
+          location: "name and address"
         });
       }
-      return res
+      return res;
     })
     .then(category => {
-      Category
-        .findById(req.body.category)
+      Category.findById(req.body.category)
         .count()
         .then(count => {
           if (count === 0) {
             return Promise.reject({
               code: 400,
-              reason: 'ValidationError',
-              message: 'Category doesn\'t exist',
-              location: 'category name'
+              reason: "ValidationError",
+              message: "Category doesn't exist",
+              location: "category name"
             });
           }
-          return count
+          return count;
         })
         .then(count => {
-          Business
-            .findByIdAndUpdate(req.params.id, {$set: theUpdate}, {new: true, runValidators: true})
+          Business.findByIdAndUpdate(
+            req.params.id,
+            { $set: theUpdate },
+            { new: true, runValidators: true }
+          )
             .then(updatedPost => res.status(204).end())
-            .catch(err => res.status(500).json({message: err.message}));
+            .catch(err => {
+              console.error(err);
+              res.status(500).json({ message: err.message });
+            });
         })
         .catch(err => {
           console.error(err);
-          if (err.reason === 'ValidationError') {
+          if (err.reason === "ValidationError") {
             return res.status(err.code).json(err);
           }
-          res.status(500).json({code: 500, message: 'Internal server error'});
+          res.status(500).json({ code: 500, message: "Internal server error" });
         });
     })
     .catch(err => {
       console.error(err);
-      if (err.reason === 'ValidationError') {
+      if (err.reason === "ValidationError") {
         return res.status(err.code).json(err);
       }
-      res.status(500).json({code: 500, message: 'Internal server error'});
+      res.status(500).json({ code: 500, message: "Internal server error" });
     });
-})
-
+});
 
 // ---- Require jwtAuth later ----
 // DELETE request to delete a business
-router.delete('/:id', (req, res) => {
-  Business
-    .findById(req.params.id)
+router.delete("/:id", (req, res) => {
+  Business.findById(req.params.id)
     .then(business => business.remove())
     .then(business => res.status(204).end())
-    .catch(err => res.status(500).json({message: 'Internal server error'}));
+    .catch(err => res.status(500).json({ message: "Internal server error" }));
 });
 
-module.exports = {router};
+module.exports = { router };
