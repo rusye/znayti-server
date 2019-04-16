5fa2c3e7fcfae28a9c4e332a7f5ae6d54f696f5f
Russ yev <ruslanyev@yahoo.com>
2019-04-16 00:32:24 -0700

    added required fields for submissions, made emails change formats if edit or add submission

--- mailRouter.js
+++ mailRouter.js
@@ -15,17 +15,55 @@ const {
 } = require("./config");
 
 router.post("/", (req, res) => {
-  let contactName = req.body.contactName
-    ? req.body.contactName
-    : "No specific contact";
+  let requiredFields = ["formID", "replyTo", "submitterName"];
 
-  let newBusinessDetails = `<p>
-      Name: ${req.body.businessToAdd}<br />
-      Telephone: ${req.body.newBusinessTelephone}<br />
-      Contact: ${contactName}<br />
-      Address: ${req.body.newBusinessAddress}<br />
-      Hours: ${req.body.newBusinessHours}
-    </p>`;
+  {
+    req.body.formID === "edit"
+      ? requiredFields.push("businessID", "comment")
+      : null;
+  }
+
+  requiredFields.forEach(field => {
+    if (!(field in req.body)) {
+      const message = `Missing "${field}" in request body`;
+      console.error(message);
+      return res.status(400).json({ code: 400, message });
+    }
+  });
+
+  let userSubject =
+    req.body.formID === "add"
+      ? `Your Submission ${req.body.businessToAdd}`
+      : `Your Edit Request For ${req.body.businessToEdit}`;
+
+  let adminSubject =
+    req.body.formID === "add"
+      ? `New Submission ${req.body.businessToAdd}`
+      : `New Edit Request For ${req.body.businessToEdit}`;
+
+  let comment = req.body.comment ? req.body.comment : null;
+
+  let userIntroHtml =
+    req.body.formID === "add"
+      ? `<p>${req.body.submitterName},</p>
+    <p>We will be adding ${req.body.businessToAdd} to Znayti soon.`
+      : `<p>${req.body.submitterName},</p>
+    <p>We will be reviewing your edit request for ${req.body.businessToEdit}.`;
+
+  let adminIntroHtml =
+    req.body.formID === "add"
+      ? "Please add this listing"
+      : "Please review this edit request";
+
+  let submissionDetails =
+    req.body.formID === "add"
+      ? `Name: ${req.body.businessToAdd}<br/>
+        Telephone: ${req.body.newBusinessTelephone}<br/>
+        Address: ${req.body.newBusinessAddress}<br/>
+        Hours: ${req.body.newBusinessHours}<br/>
+        Comment: ${comment}`
+      : `Name: ${req.body.businessToEdit}<br/>
+        Comment: ${comment}`;
 
   let transporter = nodemailer.createTransport({
     host: "smtp.zoho.com",
@@ -43,12 +81,9 @@ router.post("/", (req, res) => {
     {
       from: `Znayti Support <${SUPPORT_EMAIL}>`,
       to: `${req.body.submitterName} <${req.body.replyTo}>`,
-      subject: `Your submission ${req.body.businessToAdd}`,
-      html: `<p>${req.body.submitterName},</p>
-        <p>We will be adding ${
-          req.body.businessToAdd
-        } to Znayti soon.  If you have any questions please feel free to reach out.</p>
-        <p>${newBusinessDetails}</p>
+      subject: `${userSubject}`,
+      html: `${userIntroHtml}  If you have any questions please feel free to reach out.</p>
+        <p>${submissionDetails}</p>
         <p>Znayti - Helping you connect with a business that can communicate with you in Russian.</p>`
     },
     {
@@ -56,10 +91,16 @@ router.post("/", (req, res) => {
       to: `Znayti Support <${SUPPORT_EMAIL}>`,
       bcc: `Notification <${ALERT_EMAIL}>`,
       replyTo: `${req.body.replyTo}`,
-      subject: `New Submission ${req.body.businessToAdd}`,
-      html: `<h1>Please add this listing</h1><p>${newBusinessDetails}</p>`
+      subject: `${adminSubject}`,
+      html:
+        `<h1>${adminIntroHtml}</h1><p>${submissionDetails}<br />` +
+        (req.body.formID === "edit"
+          ? `Business ID: ${req.body.businessID}`
+          : `</p>`)
     }
   ];
+
+  res.status(201).json({ message: "Email successfully sent" });
 
   mailOptions.forEach(mail => {
     transporter.sendMail(mail, function(err, info) {
@@ -69,7 +110,7 @@ router.post("/", (req, res) => {
           .status(500)
           .json({ message: "Something went wrong, please try again later" });
       } else {
-        res.status(201).json({ message: "Email successfully send" });
+        res.status(201).json({ message: "Email successfully sent" });
       }
     });
   });
